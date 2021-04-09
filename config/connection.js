36a6d8c819afee9981d9a64f8aef87d23a950d3f
Mariah Rivera <rivmariah06@gmail.com>
2021-04-08 23:18:41 -0400

    moving code to correct file connection.js

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db",
+  });
+}
+
+// Make connection.
+connection.connect();
+// Export connection for our ORM to use.
+module.exports = connection;
