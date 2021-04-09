0eddbe9768a853bfcd932f5089cb525330ad6173
Mariah Rivera <rivmariah06@gmail.com>
2021-04-08 22:15:51 -0400

    debugging and correcting files

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller");
+var routes = require("./controllers/burgers_controllers");
 
 app.use(routes);
 
